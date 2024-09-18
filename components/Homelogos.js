85c667c8b9fa6acc3b322cf88ebf1f5c1645af28
musaabMD <mousab.r@outlook.com>
2024-09-18 17:35:41 +0300

    “f”

--- components/Homelogos.js
+++ components/Homelogos.js
@@ -2,6 +2,7 @@
 
 import React, { Suspense, useState } from 'react';
 import { createClient } from '@/libs/supabase/client'; // Adjust the path to your Supabase client setup
+
 const getLogoUrl = (apiName) => {
   return `https://img.logo.dev/${apiName}?token=pk_f8BWa9CoSCOyj527NcZ2LA`;
 };
@@ -38,8 +39,7 @@ const manualCompanies = [
     { inc_api_name: 'Calendly.com' },
     { inc_api_name: 'spotify.com' },
     { inc_api_name: 'shopify.com' },
-
-  // Add more companies as needed
+    // Add more companies as needed
 ];
 
 export default function Logos() {
@@ -47,27 +47,25 @@ export default function Logos() {
 
   return (
     <>
-
-    <Suspense>
-        
-    <div className="py-24 lg:py-16 bg-transparent dark:bg-transparent"> {/* Use transparent background */}
-      <div className="mx-auto max-w-2xl px-6 lg:px-4">
-        <div className="-mx-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 overflow-hidden sm:mx-0 sm:rounded-3xl">
-          {companies.map((company, index) => (
-            <div key={index} className="p-2"> {/* Removed bg-white for transparency */}
-              <img
-                alt={company.inc_api_name}
-                src={getLogoUrl(company.inc_api_name)}
-                width={600}  // Increase the width to make it larger
-                height={300} // Increase the height accordingly
-                className="max-h-24 w-full object-contain" // Adjust max height for better visibility
-              />
+      <Suspense>
+        <div className="py-24 lg:py-16 bg-transparent dark:bg-transparent"> {/* Use transparent background */}
+          <div className="mx-auto max-w-2xl px-6 lg:px-4">
+            <div className="-mx-2 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 overflow-hidden sm:mx-0 sm:rounded-3xl">
+              {companies.map((company, index) => (
+                <div key={index} className="p-2"> {/* Removed bg-white for transparency */}
+                  <img
+                    alt={company.inc_api_name}
+                    src={getLogoUrl(company.inc_api_name)}
+                    width={600}  // Increase the width to make it larger
+                    height={300} // Increase the height accordingly
+                    className="max-h-24 w-full object-contain" // Adjust max height for better visibility
+                  />
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-    </div>
-    </Suspense>
-</>
+      </Suspense>
+    </>
   );
 }
